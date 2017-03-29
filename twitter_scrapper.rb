3049d491e2e474802c34230bcea9f950d11f37a5
doberman7 <ivanrubioangeles@gmail.com>
2017-03-28 21:05:14 -0600

    twitter scrapper terminado, falta comentar

--- twitter_scrapper.rb
+++ twitter_scrapper.rb
@@ -1,48 +1,52 @@
 #para activar usar:
 #ruby twitter_scrapper.rb https://twitter.com/Doberman_7
-require 'open-uri' #webpage is live on a remote site twiter
-require 'Nokogiri'
+require 'open-uri'
+ #webpage is live on a remote site twiter
+require 'nokogiri'#Esta es la biblioteca para interactuar con un HTML
 class TwitterScrapper
-  def initialize(url)
-		@page = Nokogiri::HTML(open(url))
+  def initialize(url)#initializar el parametro "url"
+		@page = Nokogiri::HTML(open(url))# el url es convertido en un una instance_var del objeto TwitterScrapper
   end
 
   def extract_username
+    #buscar en la variables de instancia 
 		profile_name = @page.search(".ProfileHeaderCard-name > a")# El "> a" hacen no visible el espacio de linea en la consola
-		"Username: #{profile_name.inner_text}"
+		puts "Username: #{profile_name.inner_text}"#imprimir la el
+    puts "-" * 60
   end
 
   def extract_tweets
-		tweet = @page.search(".tweet")# El parametro ".ProfileNav-value"  se obtiene con el inspector de Navegador
-		"#{tweet[0].inner_text}"
+		tweet = @page.search(".js-tweet-text-container")# El parametro ".ProfileNav-value"  se obtiene con el inspector de Navegador
+    date = @page.search("._timestamp")
+
+    counter = 0
+    puts"Tweets:"
+      tweet.first(2).each do |t|
+        date.first(2).each do |d|
+            puts " #{d.inner_text}:#{t.inner_text.strip}"
+            resp = @page.search(".ProfileTweet-actionCount")[counter].inner_text.strip#8
+            reTweet = @page.search(".ProfileTweet-actionCount")[counter+=1].inner_text.strip#9
+            likes = @page.search(".ProfileTweet-actionCount")[counter+=1].inner_text.strip#10
+            puts resp
+            puts reTweet
+            puts likes
+            counter += 6
+            puts "-" * 50
+            break
+        end
+      end
   end
 
   def extract_stats
 		profile_tweet = @page.search(".ProfileNav-value")# El parametro ".ProfileNav-value"  se obtiene con el inspector de Navegador
-		"Tweets: #{profile_tweet[0].inner_text} \nSiguiendo: #{profile_tweet[1].inner_text}\nSeguidores #{profile_tweet[2].inner_text}\nLikes #{profile_tweet[3].inner_text} "
+    puts "Tweets: #{profile_tweet[0].inner_text} Siguiendo: #{profile_tweet[1].inner_text} Seguidores #{profile_tweet[2].inner_text} Likes #{profile_tweet[3].inner_text} "
+    puts "-" * 50
   end
 
 end
 url = ARGV
 instance = TwitterScrapper.new(url.join)
 
-puts instance.extract_username
-puts instance.extract_tweets
-puts instance.extract_stats
-
-=begin
-$ ruby twitter_scrapper.rb
-Username: Chicharito Hernandez
-----------------------------------------------------------------------------
-Stats: Tweets: 734, Siguiendo: 298, Seguidores: 4,95M, Favoritos: 70
-----------------------------------------------------------------------------
-Tweets:
-30 de mar.: Somos futbolistas unidos por una gran causa. Entendamos juntos al autismo @iluminemosazul https://www.youtube.com/watch?v=g3umuOWdMyA … #Iluminemosdeazul
-Retweets:862, Favorites:1,6K
-
-30 de mar.: Felicidades churuuuuuu @SergioRamos que la pases muy bien!!!  #top pic.twitter.com/ulhTMRk38V
-Retweets:2,6K, Favorites:5,3K
-
-29 de mar.: Fe y confianza!!!... Muchísimas gracias por el gran apoyo en LA!... #blessed pic.twitter.com/oOiEmYhKQt
-Retweets:3,3K, Favorites:7K
-=end
+instance.extract_username
+instance.extract_stats
+instance.extract_tweets
