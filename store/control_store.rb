d1e261e26410d06579aead772f59ae780187ed15
doberman7 <ivanrubioangeles@gmail.com>
2017-04-06 14:56:34 -0500

    update store

--- store/control_store.rb
+++ store/control_store.rb
@@ -22,9 +22,9 @@ class Controller
     log = nil
     until log == :true_admin || log == :true_user
 
-      @view.log_view(@input.to_i)
+      @view.log_view(@input.to_i)#"enter email"
       log = gets.chomp
-      case
+        case
         when log == @admin.email
           @view.log_view(3)
           until log == @admin.password
@@ -32,35 +32,46 @@ class Controller
             break if log == "exit"
             @view.log_view(5) if log != @admin.password #|| log != "exit"
           end
-          if log != "exit" || log == @admin.password
+          if log != "exit" && log == @admin.password
             @view.log_view(6)
             log = :true_admin
           end
-        when log != @admin.email
+        when log == "menu"
+          @view.welcoming
+          @input = gets.chomp
+          menu(@input)
+        else
           all_users = @admin.user_index
           all_users.each do |user_data|
             if user_data[1] == log#SI el email del arreglo == al input
               @view.log_view(7) #welcomin user
-              until log == user_data[2]#AQUI
-                @view.log_view(3) #password
+              @view.log_view(3)
+              until user_data[2] == log
                 log = gets.chomp
-                if log == user_data[2]#input == password
-                  log = :true_user
-                end
+                break if log == "exit"
+                @view.log_view(5) if user_data[2] != log
+              end
+              if log != "exit" && log == user_data[2]
+                @view.log_view(7)
+                log = :true_user
               end
             end
           end
-      end#end of case
+        end#end of if
     end#end of until
-    $log_result = log
-  end
+    p $log_result = log
+  end#end of login method
+
   def register
-    @view.reg_view(@input.to_i)
+    @view.reg_view(1)
     @buyer.email = gets.chomp
-    @view.reg_view(3)
+    @view.reg_view(2)
     @buyer.password = gets.chomp
     @buyer.add_user(@buyer.email,@buyer.password)
-    @view.reg_view(4)
+    @view.reg_view(3)
+  end
+  def menu_admin
+
   end
 
 end
--- store/view_store.rb
+++ store/view_store.rb
@@ -7,23 +7,23 @@ class View
   def log_view(input)
     case input
       when 1 then puts "Enter email"
-      when 2 then puts "or enter \"exit\" to go back "
+      #when 2 then puts "or enter \"exit\" to go back "
       when 3 then puts "Enter pasword"
-      when 4 then puts "Unknown email, try again"
+      when 4 then puts "Unknown email, try again,  or enter \"exit\" to go back "
       when 5 then puts "rong password, try again, or enter \"exit\" to go back "
       when 6 then puts "Welcome Administrador. Your type is Admin."
       when 7 then puts "Welcome User."
       #when 8 then puts "Select option number:\n1. Logout\n2. Products index\n"
     end
   end
-  def reg_view(imethod_namenput)
+  def reg_view(input)
    case input
-   when 2 then puts "Register Email"
-   when 3 then puts "Register pasword"
-   when 4 then puts "user added"
+     when 1 then puts "Record  email"
+     when 2 then puts "Record pasword"
+     when 3 then puts "user added"
    end
   end
-  def ind_view(input)
+  def menu_view(input)
 
   end
 end
