9c86e1eed9cd7bb791bf6e4665f6e84f4697e135
doberman7 <ivanrubioangeles@gmail.com>
2017-04-19 12:22:57 -0500

    update: first funtional implementation

--- maraton/controler_maraton.rb
+++ maraton/controler_maraton.rb
@@ -7,11 +7,30 @@ class Controler
     @deck = Deck.new
     @view = View.new
     #@view.welcoming
-    get_items
+    move_through_the_questions
   end
-  def get_items
-    item = @item.get_it(0)
-    p item.question
+  def move_through_the_questions
+    item_n = 0
+    correct = 0
+    rong = 0
+    while item_n <= 15
+      item = get_items(item_n)
+      @view.show(item.question)
+      @view.show(item.answer)
+      if item.answer == @view.user_input
+        @view.rigth_or_rong(true)
+        correct += 1
+      else
+        @view.rigth_or_rong(false) ;
+        rong += 1
+      end
+      item_n += 1
+    end
+    @view.veredict(correct,rong)
+  end
+
+  def get_items(n)
+    @item.get_it(n)
   end
 
 end
--- maraton/view_maraton.rb
+++ maraton/view_maraton.rb
@@ -2,7 +2,17 @@ class View
 	def welcoming
 		 puts "Bienvenido a maraton \n te daremos una pregunta y deberas adivinar la respuesta correcta"
 	end
-	def show_question(item)
+	def show(item)
 		puts "#{item}"
+	end
+	def user_input
+		answer_user = gets.chomp
+		answer_user
+	end
+	def rigth_or_rong(so)
+		puts so == true ? "tu respuesta es CORRECTA" : "tu respuesta es INCORRECTA"
+	end
+	def veredict(correct,rong)
+		puts "Tuviste #{correct} correctas y #{rong} malas"
 	end
 end
