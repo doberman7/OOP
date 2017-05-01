938f4fcde1060bf077dda53d0331f765cdfe3bbb
doberman7 <ivanrubioangeles@gmail.com>
2017-04-30 21:27:10 -0500

    Pregúntale A La Base De Datos , DONe?

--- preguntale_BD/sentenciasPrimerBD.sql
+++ preguntale_BD/sentenciasPrimerBD.sql
@@ -41,13 +41,36 @@ SELECT Customers.CompanyName, COUNT(Orders.OrderID) FROM Orders
   ORDER BY COUNT(Orders.OrderID) DESC--ordenar por medio de la cuenta en TABLE "orders", columna "orderID"
   LIMIT 1;--limit el display a un valor, dado que esta ordenado en forma descendente, el resultado funciona.
 
-  --La suma total de piezas que fueron ordenadas por cada compañía.
-  SELECT Customers.CompanyName, OrderDetails.Quantity FROM  OrderDetails,Orders,Customers
-  GROUP BY Customers.CompanyName
-    LEFT JOIN Orders
+  --7)La suma total de piezas que fueron ordenadas por cada compañía.
+  --OrderDetails,Orders,
+  SELECT CompanyName,SUM(OrderDetails.Quantity) FROM  Customers
+  LEFT JOIN Orders
+    ON Customers.CustomerID=Orders.CustomerID
+  LEFT JOIN OrderDetails
+    ON OrderDetails.OrderID=Orders.OrderID
+    GROUP BY Customers.CompanyName
+    ORDER BY SUM(OrderDetails.Quantity) DESC
   ;
-
-
+--8)El monto total en pesos de la orden con ID 4
+SELECT orderID ,UnitPrice*Quantity FROM OrderDetails
+WHERE orderID=4
+GROUP BY OrderID
+;
+--9)El monto total en pesos de cada orden
+--CORROBORAR CON ABEL
+SELECT orderID ,UnitPrice*Quantity FROM OrderDetails
+GROUP BY OrderID
+;
+--10)La consulta del inciso anterior pero únicamente mostrando aquellas ordenes que sean mayores a $1,000.00 pesos
+--CORROBORAR CON ABEL
+SELECT orderID , UnitPrice*Quantity FROM OrderDetails
+WHERE UnitPrice*Quantity > 1000
+GROUP BY OrderID
+;
+--El total de piezas en cada orden
+SELECT OrderID, SUM(Quantity) FROM OrderDetails
+GROUP BY OrderID
+;
 --SCHEMA:
 CREATE TABLE Customers (
  CustomerID INTEGER PRIMARY KEY
@@ -72,7 +95,3 @@ CREATE TABLE OrderDetails (
 ,UnitPrice REAL
 ,Quantity INTEGER
 );
---El monto total en pesos de la orden con ID 4
---El monto total en pesos de cada orden
---La consulta del inciso anterior pero únicamente mostrando aquellas ordenes que sean mayores a $1,000.00 pesos
---El total de piezas en cada orden
