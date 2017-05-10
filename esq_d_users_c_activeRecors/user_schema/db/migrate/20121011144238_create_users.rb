efde076109c46a3c866c8242cb58805db97d7ccc
codeamac19 <codeamac19@gmail.com>
2017-05-09 20:43:37 -0500

    Esquema De Usuarios Con Active Record: UPDATE, trabajando las migraciones

--- esq_d_users_c_activeRecors/user_schema/db/migrate/20121011144238_create_users.rb
+++ esq_d_users_c_activeRecors/user_schema/db/migrate/20121011144238_create_users.rb
@@ -1,2 +1,14 @@
-# Este archivo debe contener una migración 
-
+# Este archivo debe contener una migración
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :gender
+      t.string :email
+      t.string :phone
+      t.date   :birthday      
+      t.timestamps
+    end
+  end
+end
