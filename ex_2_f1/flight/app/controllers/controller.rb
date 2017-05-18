844187e7d7a887d9a8e78ce3ac3783d47b938be1
codeamac19 <codeamac19@gmail.com>
2017-05-17 20:59:51 -0500

    Ex_2_f1: determinando cantidad de pasajeros

--- ex_2_f1/flight/app/controllers/controller.rb
+++ ex_2_f1/flight/app/controllers/controller.rb
@@ -3,21 +3,21 @@ class Controller
   def initialize(args)
     @argv_ary = args
     @view = View.new
-    index(@argv_ary)
+    index
   end
 
 
-  def index(argv)
-    number_of_option = argv[0]
+  def index
+    number_of_option = @argv_ary[0]
     if number_of_option == nil
       @view.welcome
       @view.index
-      number_of_option = @view.admin(:user_unput)
+      number_of_option = @view.input
     end
     case number_of_option
       when "" then index(number_of_option)
-      when "1" then booking
-      when "2" then admin
+      when "1" then find_flight
+      when "2" then admin_check
       when "3" then out
       else
         @view.error(0)
@@ -25,10 +25,31 @@ class Controller
 
   end
 
-  def booking
-    p 'booking'
+  def find_flight
+    @view.find_flight(:welocome_find_f)
+    selected = @view.find_flight(:input)
+    case selected
+      when "1"
+        @view.find_flight(:find_my_ticket)
+        num_flight = @view.find_flight(:input)
+        num_flight = num_flight.to_i
+        founded_plane = Flight.where(num_flight: num_flight)
+        #si ningun vuelo tiene ese numero de vuelo entonces muestra error, por el contrario si se encuentra el vuelo el mismo es mostrado
+        founded_plane.empty? ?   @view.error(:no_find_flight_founded): @view.find_flight(founded_plane);
+        num_of_passangers = @view.find_flight(:chose_num_of_passengers)
+        num_of_passangers = num_of_passangers.to_i
+        #-AQUI
+        # founded_plane.each do |plane_data|
+        #   puts founded_plane.passengers > num_of_passangers
+        # end
+        #----------
+        find_flight
+      when "2" then out
+    end
+
   end
-  def admin
+
+  def admin_check
     @view.admin(:welcome_adm)
     @view.admin(:ask_user)
     user_input_name = @view.admin(:admin_input)
@@ -39,37 +60,55 @@ class Controller
     # .empty? Returns true  if there are no records. its a methof from an ActiveRecord::Relation object
     #but if there are any records then....
     unless admin_founded.empty? == true
-      @view.admin(:menu_admin)
-      case @view.admin(:admin_input)
-        when "1" then @view.admin(:view_all_flight)
-        when "2" then @view.admin(:view_all_bookings)
-        when "3" then
-          @view.admin(:ask_num_flight)
-          num_flight = @view.admin(:admin_input)
-          @view.admin(:ask_date)
-          date = @view.admin(:admin_input)
-          depart = @view.admin(:admin_input)
-          from = @view.admin(:admin_input)
-          to = @view.admin(:admin_input)
-          duration = @view.admin(:admin_input)
-          cost = @view.admin(:admin_input)
-          passengers = @view.admin(:admin_input)
-
-          Flight.create(
-          num_flight: num_flight, date: date, depart: depart, from: from, to: to, duration: duration, cost: cost, passengers: passengers
-          )
-        when "4" then @view.admin(:admin_goodbay)
-      end
+      admin_menu
+      else @view.error(:invalid_user)
     end
-
   end
 
+  def admin_menu
+    @view.admin(:menu_admin)
+    case @view.admin(:admin_input)
+      when "1" then @view.admin(:view_all_flight)
+        admin_menu
+      when "2" then @view.admin(:view_all_bookings)
+        admin_menu
+      when "3" then add_flight
+        admin_menu
+      when "4" then @view.admin(:admin_goodbay)
+        index
+    end
+  end
 
   def out
-    p 'out'
+    @view.out
   end
-  def add
 
+  def add_flight
+    @view.admin(:ask_num_flight)
+    num_flight = @view.admin(:admin_input)
+    num_flight = num_flight.to_i
+    @view.admin(:ask_date)
+    date = @view.admin(:admin_input)
+    @view.admin(:ask_depart)
+    depart = @view.admin(:admin_input)
+    @view.admin(:ask_from)
+    from = @view.admin(:admin_input)
+    @view.admin(:ask_to)
+    to = @view.admin(:admin_input)
+    @view.admin(:ask_duration)
+    duration = @view.admin(:admin_input)
+    @view.admin(:ask_cost)
+    cost = @view.admin(:admin_input)
+    cost = cost.to_f
+    @view.admin(:ask_passengers)
+    passengers = @view.admin(:admin_input)
+    passengers = passengers.to_i
+
+    p Flight.create!(
+      num_flight: num_flight, date: date, depart: depart, from: from, to: to, duration: duration, cost: cost, passengers: passengers
+    )
+
+    admin_menu
   end
 
   def delete
--- ex_2_f1/flight/app/views/view.rb
+++ ex_2_f1/flight/app/views/view.rb
@@ -21,11 +21,18 @@ class View
   def update
   end
 
-	def error(number_of_error)
-    case number_of_error
-    when 0 then puts 'that option doesnt exist'
+	def error(type_of_error)
+    case type_of_error
+      when 0 then puts 'that option doesnt exist'
+      when :invalid_user then puts 'usuario no valido'
+      when :no_booking_founded then puts 'ningun vuelo con ese numero'
+
     end
 	end
+  def input
+     STDIN.gets.chomp
+  end
+
   def admin(admin_option)
     case admin_option
       when :welcome_adm then puts "Bienvenido Administrador \n#{"-"*30}"
@@ -41,7 +48,7 @@ class View
         puts '3) Crea un nuevo vuelo'
         puts '4) Salir'
         puts 'Selecciona opcion:'
-      when :admin_goodbay then puts 'adios'
+      when :admin_goodbay then puts 'adios admin'
       when :view_all_flight then  Flight.all.each do |f|
         p f
       end
@@ -49,13 +56,59 @@ class View
         p b
       end
       when :ask_num_flight then puts 'ingresa numero de vuelo'
-      when :ask_date then 'ingresa fecha'
-      when :ask_depart then 'ingresa fecha de salida'
-      when :ask_from then 'ingresa origen de vuelo'
-      when :ask_to then 'ingresa destino de vuelo'
-      when :ask_duration then 'ingresa duracion de vuelo'
-      when :ask_cost then 'ingresa costo'
-      when :ask_passengers then 'ingresa pajeros'
+      when :ask_date then puts 'ingresa FECHA (aaaa-mm-dd)'
+      when :ask_depart then puts 'ingresa hora de SALIDA (hh:mm:ss)'
+      when :ask_from then puts 'ingresa ORIGEN de vuelo'
+      when :ask_to then puts 'ingresa DESTINO de vuelo'
+      when :ask_duration then puts 'ingresa DURACION de vuelo (hh:mm:ss)'
+      when :ask_cost then puts 'ingresa COSTO (i.f)'
+      when :ask_passengers then puts 'ingresa numero de pajeros'
+
     end
+  end
+
+  def find_flight(value)
+    case value
+      when :welocome_find_f
+        puts "-" * 50
+        puts '!!!Bienvenido!!!'
+        puts "-" * 50
+        puts "1) Encuentra tu boleto de avion"
+        puts "2) Salir"
+        puts "Selecciona una opcion:"
+      when :input then STDIN.gets.chomp
+      when :find_my_ticket
+        Flight.all.each do |f|
+          puts "Numero de Vuelo:#{f.num_flight}"
+          puts "Fecha:#{f.date}"
+          puts "Salida:#{f.depart}"
+          puts "Origen:#{f.from}"
+          puts "Destino:#{f.to}"
+          puts "Duracion:#{f.duration}"
+          puts "Costo:#{f.cost}"
+          puts "Pasajeros:#{f.passengers}"
+          puts "*" * 30
+        end
+        puts 'Selecciona tu numero vuelo:'
+      when :chose_num_of_passengers
+        puts 'ingresa numero de pasajeros'
+        STDIN.gets.chomp
+      else
+        value.each do |f|
+          puts "Numero de Vuelo:#{f.num_flight}"
+          puts "Fecha:#{f.date}"
+          puts "Salida:#{f.depart}"
+          puts "Origen:#{f.from}"
+          puts "Destino:#{f.to}"
+          puts "Duracion:#{f.duration}"
+          puts "Costo:#{f.cost}"
+          puts "Pasajeros:#{f.passengers}"
+        end
+
+    end
+  end
+
+  def out
+    puts "adios"
   end
 end
